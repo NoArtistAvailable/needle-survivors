fc2e71937817d78181afe51b1bd3b946e75e26cb
NoArtistAvailable <42377289+NoArtistAvailable@users.noreply.github.com>
2023-09-15 19:32:10 +0200

    removed default orbit cam

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,20 @@
-import { GameObject, NeedleEngine } from '@needle-tools/engine';
-import { AxesHelper, GridHelper } from 'three';
+import { GameObject, NeedleEngine, OrbitControls } from '@needle-tools/engine';
+import { AxesHelper, GridHelper, MathUtils } from 'three';
 import * as THREE from 'three';
 import { PlayerController } from './modules/input/PlayerController';
 
 NeedleEngine.addContextCreatedCallback((args) => {
-  const context = args.context;
+  const context = args.context!;
   const scene = context.scene;
+  const camera = context.mainCameraComponent!;
+
+  camera.gameObject.getComponent(OrbitControls)!.destroy();
+  camera.gameObject.position.set(0, 20, 8);
+  camera.gameObject.rotation.set(-70 * MathUtils.DEG2RAD, 0, 0);
+  camera.fieldOfView = 45;
 
   const grid = new GridHelper();
   scene.add(grid);
-
-  const axis = new AxesHelper();
-  axis.position.y = 1;
-  scene.add(axis);
 
   const geometry = new THREE.BoxGeometry(1, 1, 1);
   const material = new THREE.MeshStandardMaterial({ color: 0xdddddd });
--- src/modules/input/PlayerController.ts
+++ src/modules/input/PlayerController.ts
@@ -14,12 +14,15 @@ export class PlayerController extends Behaviour {
   update() {
     this.workVector.copy(this.context!.mainCameraComponent!.forward);
     this.workVector.y = 0;
+    this.workVector.normalize();
     this.workVector.multiplyScalar(this.forwardInput.value);
     this.workVector.multiplyScalar(this.speed * this.context.time.deltaTime);
     this.gameObject.position.add(this.workVector);
+
     this.workVector.copy(this.context!.mainCameraComponent!.forward);
     this.workVector.cross(new THREE.Vector3(0, 1, 0));
     this.workVector.y = 0;
+    this.workVector.normalize();
     this.workVector.multiplyScalar(this.rightInput.value);
     this.workVector.multiplyScalar(this.speed * this.context.time.deltaTime);
     this.gameObject.position.add(this.workVector);
